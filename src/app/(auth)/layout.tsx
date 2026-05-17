53f686a7ea7cdee34fb1863635226c105f4d5293
haseebarshad17 <haseebarshad1712@Gmail.com>
2026-05-17 08:10:37 -0700

    REFACTOR: Metadata adjust the template + absolute meta for auth and default literals for app

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,8 +8,9 @@ interface AuthLayoutProps {
 }
 
 export const metadata: Metadata = {
-    title: "Deenify - Sign in or Sign up",
-    description: "Sign in to your Deenify account or create a new one to continue your Quran, Hadith, guides and learning journey.",
+    title: { absolute: "Deenify - Sign in or Sign up" },
+    description: `Sign in to your Deenify account or create a new one to continue your Quran, 
+    Hadith, guides and learning journey.`,
 }
 
 
--- src/assets/meta.ts
+++ src/assets/meta.ts
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 const siteTitle = "Deenify";
+const subTitle = "Daily Islamic Lifestyle App";
 const siteUrl = "https://deenify.com";
 const description = `Your daily Islamic lifestyle app — Quran, Hadith, Adhan, Qibla, and more. 
 Quran, Prayer Times, Qibla Direction, and more — all in one Deenify Islamic app.`;
@@ -8,8 +9,8 @@ const ogImage = `${siteUrl}/og.png`;
 
 export const defaultMeta: Metadata = {
     title: {
-        default: siteTitle,
-        template: `%s | ${siteTitle}`,
+        default: `${siteTitle} | ${subTitle}`,
+        template: `%s | ${siteTitle} - ${subTitle}`,
     },
     description,
     keywords: [
